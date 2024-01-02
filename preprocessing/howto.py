7aeb3cbf613859ca0f39396aa5bba5112aadff12
Gizem Göker <gizemgoker21@gmail.com>
2024-01-02 23:29:20 +0100

    delete a row on howto.py
    in preprocessing directory

--- preprocessing/howto.py
+++ preprocessing/howto.py
@@ -1,4 +1,3 @@
-print("1-Data preprocessing")
 print("2-First you need a dataset")
 print("3-check if there is missing columns/rows")
 print("4-if there is missing columns/rows, apply filling rows methods")
